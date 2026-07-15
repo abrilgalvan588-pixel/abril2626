9fb2532461ffe93221f36f77a8dfd72a2e1b470e
Abril Perez Galvan <abrilgalvan588@gmail.com>
2026-07-15 14:11:39 -0600

    Se termino de alterar o cambiar, columnas, tipos de datos, restricciones con ALTER TABLE y eliminacion de objetos de BD con DROP TABLE, especificamente con tablas y base de datos

--- 03-Construccion-BasedeDatos/02-Alter-Drop.sql
+++ 03-Construccion-BasedeDatos/02-Alter-Drop.sql
@@ -78,3 +78,75 @@ CHECK (edad>=18);
 GO
 
 -- TODO: CONTINUAR CON RESTRICCIONES DEFAULT 
+
+-- agregar una restriccion alumno 
+-- DEFAULT 
+ALTER TABLE alumno 
+ADD activo BIT NOT NULL;
+GO
+
+ALTER TABLE alumno
+ADD CONSTRAINT de_alumno_activo
+DEFAULT 1
+FOR activo;
+GO
+
+-- ELIMINAR RESTRICCIONES SE ELIMINA CON ALTER 
+-- EXEC sp_help alumno; 
+
+-- Consuta para conocer los tipos de nombre y las restricciones de una tabla
+SELECT 
+	o.name AS nombre_restriccion,
+	o.type_desc AS tipo_restriccion
+FROM sys.objects AS o
+WHERE o.parent_object_id = OBJECT_ID('alumno')
+AND o.type IN ('PK','F','UQ','C','D')
+ORDER BY o.type_desc;
+
+-- Elimiar una PK
+ALTER TABLE alumno
+DROP CONSTRAINT	pk_alumno;
+
+-- Eliminar un CHECK 
+ALTER TABLE alumno
+DROP CONSTRAINT	ck_alumno_edad;
+-- Elimar un UNIQUE 
+ALTER TABLE alumno
+ADD matricula VARCHAR (12) NOT NULL;
+
+ALTER TABLE alumno 
+ADD CONSTRAINT uq_alumno_matricula
+UNIQUE (matricula);
+
+ALTER TABLE alumno
+DROP CONSTRAINT	uq_alumno_matricula;
+
+-- Eliminar el DEFAULT
+ALTER TABLE alumno
+DROP CONSTRAINT	df_alumno_activo;
+
+-- ELIMINAR LA FK
+ALTER TABLE alumno
+DROP CONSTRAINT	fk_alumno_carrera;
+
+-- DROP (ELIMINA OBJETOS COMPLETOS CON DROP)
+-- MUESTRA LAS TABLAS DE LA BACE DE DATOS (CON SELECT NAME SE CONSULTAN LAS TABLAS)
+SELECT name
+FROM sys.tables;
+
+
+DROP TABLE alumno;
+GO
+DROP TABLE alumno2;
+GO 
+DROP TABLE carrera;
+GO
+
+USE master;
+GO
+
+SELECT *
+FROM alumno;
+
+DROP DATABASE escuelita;
+GO
